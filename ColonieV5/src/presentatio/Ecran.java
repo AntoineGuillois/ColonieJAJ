e2bd9f363da63e56aed8ac68410d485536890527
Jeremie Herve <jeremie.herve.jh@gmail.com>
2017-04-04 11:28:07 +0200

    tres

--- ColonieV5/src/presentatio/Ecran.java
+++ ColonieV5/src/presentatio/Ecran.java
@@ -6,20 +6,12 @@ public class Ecran {
 		
 		Fenetre fenetre=new Fenetre();
 	    fenetre.setVisible(true);
-<<<<<<< HEAD
-	    
-=======
-	    
->>>>>>> branch 'master' of https://github.com/AntoineGuillois/ColonieJAJ.git
+
 	    //La la la la la bla bla bla bla bla bla bla 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
+
 	    //erokf
->>>>>>> branch 'master' of https://github.com/AntoineGuillois/ColonieJAJ.git
-=======
+
 	    //erokf modification encore de la part d'Antoine !!!!!!!!!!!!!!!!!!!!!!!!!
->>>>>>> branch 'master' of https://github.com/AntoineGuillois/ColonieJAJ.git
 	}
 
 }
